0219e505b110ca4632426ed01769d307fb9b8af6
Ibán López <ibanlopezd@gmail.com>
2020-07-25 14:15:44 +0200

    Updates

---
+++ precache-manifest.108469110302b85a9b9e57c95f12a702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361ee271cc270855a0ca277524fb2acd",
+    "url": "/movieme/index.html"
+  },
+  {
+    "revision": "11403e53b0b3dd7cf5b2",
+    "url": "/movieme/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8fbf9aa3abbcff3bbbac",
+    "url": "/movieme/static/js/2.d3c4c431.chunk.js"
+  },
+  {
+    "revision": "228bac5b7587832345b091357f4a52ce",
+    "url": "/movieme/static/js/2.d3c4c431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11403e53b0b3dd7cf5b2",
+    "url": "/movieme/static/js/main.6efe05db.chunk.js"
+  },
+  {
+    "revision": "d6213a4ab6d875502d41",
+    "url": "/movieme/static/js/runtime-main.a159ac7f.js"
+  }
+]);
