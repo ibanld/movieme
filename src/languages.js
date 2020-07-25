1d9109cc438bad8994c3edb3b4e4ef31d6ea135b
Ibán López <ibanlopezd@gmail.com>
2020-07-25 12:53:03 +0200

    Created separated file for languages

---
+++ src/languages.js
@@ -0,0 +1,39 @@
+export const english = {
+	searchTitle: 'Search',
+	searchPlaceHolder: 'Look for a movie!',
+	noResults: 'Ops! There aren`t results for your search. Try again!',
+	searchNavbar: 'Search',
+	watchedNavbar: 'Watched',
+	pendingNavbar: 'Pending',
+	watchedTitle: 'Your Watched Movies',
+	pendingTitle: 'Your Pending Movies',
+	addMovie: 'Add a movie to your list',
+	alertDeletedWatch: ' deleted from your Watched movies',
+	alertAddedWatch: ' added to Watched Movies',
+	alertDeletedPending: ' deleted from your Pending movies',
+	alertAddedPending: ' added to Pending movies',
+	ratingInit: 'You have rated ',
+	ratingWith: ' with ',
+	ratingRate: ' stars'
+};
+
+export const spanish = {
+	searchTitle: 'Buscar',
+	searchPlaceHolder: 'Busca una película',
+	noResults: 'Vaya! Parece que tu búsqueda no ha dadao resultado. ¡Intentalo de nuevo!',
+	searchNavbar: 'Buscar',
+	watchedNavbar: 'Vistas',
+	pendingNavbar: 'Pendientes',
+	watchedTitle: 'Tus Películas Vistas',
+	pendingTitle: 'Tus Películas Pendientes',
+	addMovie: 'Añade una película a tu lista',
+	alertDeletedWatch: ' eliminada de tus Películas Vistas',
+	alertAddedWatch: ' añadida a Películas Vistas',
+	alertDeletedPending: ' eliminada de tus Películas Pendientes',
+	alertAddedPending: ' añadida a tus Películas Pendientes',
+	ratingInit: 'Has valorado ',
+	ratingWith: ' con ',
+	ratingRate: ' estrellas'
+};
+
+export const portuguese = {};
--- src/redux/translation.js
+++ src/redux/translation.js
@@ -1,49 +1,11 @@
+import { english, spanish, portuguese } from '../languages';
+
 export function setTranslation(lang) {
 	return {
 		type: 'SET_TRANSLATION',
 		payload: lang
 	};
 }
-
-const english = {
-	searchTitle: 'Search',
-	searchPlaceHolder: 'Look for a movie!',
-	noResults: 'Ops! There arent any results for your search. Try again!',
-	searchNavbar: 'Search',
-	watchedNavbar: 'Watched',
-	pendingNavbar: 'Pending',
-	watchedTitle: 'Your Watched Movies',
-	pendingTitle: 'Your Pending Movies',
-	addMovie: 'Add a movie to your list',
-	alertDeletedWatch: ' deleted from your Watched movies',
-	alertAddedWatch: ' added to Watched Movies',
-	alertDeletedPending: ' deleted from your Pending movies',
-	alertAddedPending: ' added to Pending movies',
-	ratingInit: 'You have rated ',
-	ratingWith: ' with ',
-	ratingRate: ' stars'
-};
-
-const spanish = {
-	searchTitle: 'Buscar',
-	searchPlaceHolder: 'Busca una película',
-	noResults: 'Vaya! Parece que tu búsqueda no ha dadao resultado. ¡Intentalo de nuevo!',
-	searchNavbar: 'Buscar',
-	watchedNavbar: 'Vistas',
-	pendingNavbar: 'Pendientes',
-	watchedTitle: 'Tus Películas Vistas',
-	pendingTitle: 'Tus Películas Pendientes',
-	addMovie: 'Añade una película a tu lista',
-	alertDeletedWatch: ' eliminada de tus Películas Vistas',
-	alertAddedWatch: ' añadida a Películas Vistas',
-	alertDeletedPending: ' eliminada de tus Películas Pendientes',
-	alertAddedPending: ' añadida a tus Películas Pendientes',
-	ratingInit: 'Has valorado ',
-	ratingWith: ' con ',
-	ratingRate: ' estrellas'
-};
-
-const portuguese = {};
 
 export default function translationReducer(setTranslation = english, action) {
 	switch (action.type) {
